f64a73958e9cff627a9ab112a58bd4811a64c46e
murdakCreations <thisisimportant@gmail.com>
2023-05-26 21:46:32 +0800

    Create Controller

--- server/app/controllers/tutorial.controller.js
+++ server/app/controllers/tutorial.controller.js
@@ -1,8 +1,34 @@
 const db = require("../models");
-const bmi = db.tutorials;
+const BMI = db.tutorials;
 const Op = db.Sequelize.Op;
 
 // Create and Save a new BMI
 exports.create = (req, res) => {
+    // Validate request
+    if(!req.body) {
+        res.status(400).send({
+            message: "Content can not be empty!"
+        })
+        return;
+    }
 
+    // Create Student BMI
+    const bmiData = {
+        studentID: req.body.studentID,
+        studentName: req.body.studentName,
+        weight: req.body.weight,
+        height: req.body.height
+    }
+
+    // Save BMI in the database
+    BMI.create(bmiData)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occured while creating the Student BMI"
+            })
+        });
 };
