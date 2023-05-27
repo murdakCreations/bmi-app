aa096dba2c918b081efc10371e4f5760284c2df8
murdakCreations <thisisimportant@gmail.com>
2023-05-27 12:44:56 +0800

    Setup nav bar and Routes frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,42 @@
-import React, { Component } from "react";
-import {Route, Routes,Link} from "react-router-dom";
+import React, {Component} from "react"
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Route, Routes, Link } from "react-router-dom"
+import "./App.css"
 
-class App extends Component {
+import AddStudentBMI from "./components/add-student.component"
+import StudentBMI from "./components/student.component"
+import StudentBMIList from "./components/students-list.component"
+
+export default class App extends Component {
   render() {
     return (
       <div>
-        <nav>
-          <a href="">
-            Home
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <a href="/" className="navbar-brand">
+            BMI Recorder
           </a>
-          <div>
-            <li>
-              <Link to={""}>
-                Student BMI
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/studentBmis"} className="nav-link">
+                Report
               </Link>
             </li>
-            <li>
-              <Link to={""}>
-                Add
+            <li className="nav-item">
+              <Link to={"/add"} className="nav-link">
+                Add Data
               </Link>
             </li>
           </div>
         </nav>
-        <div>
+        <div className="container mt-3">
           <Routes>
-            <Route path=""/>
+            <Route path="/" element={<StudentBMIList/>}/>
+            <Route path="/studentBmis" element={<StudentBMIList/>}/>
+            <Route path="/add" element={<AddStudentBMI/>}/>
+            <Route path="/studentBmis/:id" element={<StudentBMI/>}/>
           </Routes>
         </div>
       </div>
-    );
+    )
   }
 }
-
-export default App;
